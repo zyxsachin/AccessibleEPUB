8deb182522782d147b3fd4969a7bec1dfe5a6287
Sachin Rajgopal <zyxsachin@hotmail.com>
2018-07-16 13:20:53 +0200

    Tried to test markdig to have an idea of how to create a markup language for Accessible EPUB

---
+++ AccessibleEPUB/markdigTest/markdigTest/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace markdigTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.geckoWebBrowser1 = new Gecko.GeckoWebBrowser();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 98);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(510, 474);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 57);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(149, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 52);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(328, 22);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(156, 52);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.geckoWebBrowser1);
+            this.panel1.Location = new System.Drawing.Point(538, 41);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(658, 530);
+            this.panel1.TabIndex = 7;
+            // 
+            // geckoWebBrowser1
+            // 
+            this.geckoWebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.geckoWebBrowser1.FrameEventsPropagateToMainWindow = false;
+            this.geckoWebBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.geckoWebBrowser1.Name = "geckoWebBrowser1";
+            this.geckoWebBrowser1.Size = new System.Drawing.Size(658, 530);
+            this.geckoWebBrowser1.TabIndex = 0;
+            this.geckoWebBrowser1.UseHttpActivityObserver = false;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(545, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(615, 538);
+            this.richTextBox2.TabIndex = 6;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1218, 603);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private Gecko.GeckoWebBrowser geckoWebBrowser1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ AccessibleEPUB/markdigTest/markdigTest/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Markdig;
+using Gecko;
+using System.IO;
+
+using System.Net;
+
+namespace markdigTest
+{
+    public partial class Form1 : Form
+    {
+        string site;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Xpcom.Initialize("Firefox");
+            site = Path.Combine(System.Environment.CurrentDirectory, "web.html");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text = Markdig.Markdown.ToHtml(richTextBox1.Text);
+            
+            File.WriteAllText(site, richTextBox2.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //gw1.Visible = !gw1.Visible;
+            panel1.Visible = !panel1.Visible;
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //geckoWebBrowser1.Navigate("http://google.com");
+            geckoWebBrowser1.Navigate(site);
+        }
+    }
+}
